ffc1b42d727420e92866ff09d692453b2b406cb1
Taylor Beeston <beeston.taylor@gmail.com>
2023-06-04 15:51:11 -0700

    :sparkles: V1

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,55 @@
 import express, { json } from "express";
-import axios from "axios";
+import {
+    InteractionResponseType,
+    InteractionType,
+    verifyKeyMiddleware,
+} from "discord-interactions";
 import { REST } from "@discordjs/rest";
-import { Routes } from "discord-api-types/v9";
-import { OpenAIApi } from "openai";
+import {
+    Routes,
+    APIApplicationCommandOption,
+    ApplicationCommandOptionType,
+} from "discord-api-types/v10";
+import { OpenAIApi, Configuration } from "openai";
 
 const discordToken = process.env.DISCORD_TOKEN;
+const discordKey = process.env.DISCORD_APPLICATION_KEY;
 const applicationId = process.env.APPLICATION_ID;
 const guildId = process.env.GUILD_ID;
+const channelId = process.env.CHANNEL_ID;
 const openAiToken = process.env.OPENAI_API_TOKEN;
 
 if (!discordToken) throw new Error("Discord Token not set!");
+if (!discordKey) throw new Error("Discord Application Key not set!");
 if (!applicationId) throw new Error("Application ID not set!");
 if (!guildId) throw new Error("Guild ID not set!");
+if (!channelId) throw new Error("Channel ID not set!");
 if (!openAiToken) throw new Error("OpenAI Token not set!");
 
-const openAi = new OpenAIApi({ accessToken: openAiToken });
+const openAi = new OpenAIApi(new Configuration({ apiKey: openAiToken }));
 
-const commands = [
+const commands: APIApplicationCommandOption[] = [
     {
         name: "chat",
-        description: "Chat with GPT-4!",
+        description: "You tryna chat with a bot?",
         options: [
             {
                 name: "message",
-                type: "STRING",
+                type: ApplicationCommandOptionType.String,
                 description: "The message you want to send to GPT-4",
                 required: true,
+            },
+            {
+                name: "private",
+                type: ApplicationCommandOptionType.Boolean,
+                description: "Do you want this to be private? (defaults to false)",
+                required: false,
             },
         ],
     },
 ];
 
-const rest = new REST({ version: "9" }).setToken(discordToken);
+const rest = new REST({ version: "10" }).setToken(discordToken);
 
 (async () => {
     try {
@@ -48,41 +66,53 @@ const rest = new REST({ version: "9" }).setToken(discordToken);
 })();
 
 async function callGPT4(message: string): Promise<string> {
-    const response = await axios.post(
-        "https://api.openai.com/v1/engines/gpt-4/completions",
-        {
-            prompt: message,
-            max_tokens: 60,
-        },
-        {
-            headers: {
-                Authorization: `Bearer ${openAiToken}`,
-            },
-        }
-    );
+    const response = await openAi.createChatCompletion({
+        model: "gpt-4",
+        messages: [{ role: "user", content: message }],
+    });
 
-    return response.data.choices[0].text;
+    return response.data.choices[0]?.message?.content ?? "";
 }
 
 // Set up the Express server for handling interactions
 const app = express();
 
-// body-parser middleware is used to parse the request body
-app.use(json());
+app.use(verifyKeyMiddleware(discordKey));
 
 app.post("/interactions", async (req, res) => {
-    if (req.body.type === 1) {
-        res.json({ type: 1 });
-    } else if (req.body.type === 2) {
-        if (req.body.data.name === "chat") {
+    const { type, data, member } = req.body;
+
+    console.log("Got a request!", { type, data, member: JSON.stringify(member) });
+
+    if (type === InteractionType.PING) {
+        return res.json({ type: InteractionResponseType.PONG });
+    }
+
+    if (type === InteractionType.APPLICATION_COMMAND) {
+        if (data.name === "chat") {
             const message = req.body.data.options[0].value;
+            const isPrivate = req.body.data.options[1]?.value;
+
+            console.log({ message });
+
             const gpt4Response = await callGPT4(message);
-            res.json({
-                type: 4,
-                data: {
-                    content: gpt4Response,
-                },
+
+            console.log({ gpt4Response });
+
+            if (!isPrivate) {
+                const content = `${member?.user?.username} asked ${message}\n\n${gpt4Response}`;
+
+                await rest.post(Routes.channelMessages(channelId), {
+                    body: { content },
+                });
+            }
+
+            return res.json({
+                type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+                data: { content: gpt4Response },
             });
         }
     }
 });
+
+export default app;
